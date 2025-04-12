27182f8e7b3e26305c9e7217b325066dc5b5fe0f
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-12 12:29:46 -0400

    refactor: format and rename create task usecase and test

--- src/todo_clean/layer1/usecase/create_task.py
+++ src/todo_clean/layer1/usecase/create_task.py
@@ -1,11 +1,11 @@
 from ...layer0.entity.task import Task
 from ..repository.i_task_repo import ITaskRepo
 
-class CreateNewTask():
+
+class CreateNewTask:
     def __init__(self, task: Task, task_repo: ITaskRepo):
         self.task = task
         self.task_repo = task_repo
 
     def execute(self):
         return self.task_repo.new_task(self.task.description)
-
--- tests/test_usecase/test_create_new_task.py
+++ tests/test_usecase/test_create_new_task.py
@@ -1,28 +1,29 @@
-from src.todo_clean.layer1.usecase.create_new_task import CreateNewTask
-from src.todo_clean.layer1.repository.i_task_repo import ITaskRepo
 from src.todo_clean.layer0.entity.task import Task
+from src.todo_clean.layer1.repository.i_task_repo import ITaskRepo
+from src.todo_clean.layer1.usecase.create_task import CreateNewTask
 
-
-class SpyTaskRepo(ITaskRepo):
-    description: str
-
-    def new_task(self, description: str) -> Task:
-        self.description = description
-        return Task(1, description)
-
-    def get_task_by_id(self) -> Task:
-        pass
-
-    def get_tasks(self) -> list[Task]:
-        pass
-
-    def edit_task_by_id(self, id_: int) -> Task:
-        pass
-
-    def delete_task(self, id_: int) -> bool:
-        pass
 
 def test_usecase_create_new_task():
+
+    class SpyTaskRepo(ITaskRepo):
+        description: str
+
+        def new_task(self, description: str) -> Task:
+            self.description = description
+            return Task(1, description)
+
+        def get_task_by_id(self, id_: int) -> Task:
+            pass
+
+        def get_tasks(self) -> list[Task]:
+            pass
+
+        def edit_task_by_id(self, id_: int) -> Task:
+            pass
+
+        def delete_task_by_id(self, id_: int) -> bool:
+            pass
+
     task = Task(1, "Random Task")
     task_repo = SpyTaskRepo()
     usecase = CreateNewTask(task, task_repo)
