3c67c39f51c7cfd3e9f6779ecbc6b3e06e3cdfcc
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 09:02:36 -0400

    feat: introduce an interface for Task entity

--- src/todo_clean/layer0/entity/task.py
+++ src/todo_clean/layer0/entity/task.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
+from typing import Protocol
+
+
+class ITask(Protocol):
+    id_: int
+    description: str
 
 
 @dataclass
-class Task:
+class Task(ITask):
     id_: int
     description: str
     # title: str
