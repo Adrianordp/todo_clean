d246dcf23c65d54da7bf8edd8e533f2060032258
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 08:16:01 -0400

    tests: refact create task use case test to use a presenter

--- tests/test_usecase/test_create_new_task.py
+++ tests/test_usecase/test_create_new_task.py
@@ -4,16 +4,19 @@ from src.todo_clean.layer1.usecase.create_task import (
     CreateTask,
     CreateTaskInputData,
     CreateTaskOutputData,
+    ICreateTaskPresenter,
 )
 
 
 def test_usecase_create_new_task():
 
-    class SpyTaskRepo(ITaskRepo):
-        description: str
+    class TaskRepoSpy(ITaskRepo):
+        new_task_called = False
+        new_task_called_with = {}
 
         def new_task(self, description: str) -> Task:
-            self.description = description
+            self.new_task_called = True
+            self.new_task_called_with = description
             return Task(1, description)
 
         def get_task_by_id(self, id_: int) -> Task:
@@ -28,12 +31,26 @@ def test_usecase_create_new_task():
         def delete_task_by_id(self, id_: int) -> bool:
             pass
 
-    description = "Random Task"
-    input_data = CreateTaskInputData(description)
-    task_repo_spy = SpyTaskRepo()
-    usecase = CreateTask(task_repo_spy)
-    response = usecase.execute(input_data)
+    class CreateTaskPresenterSpy(ICreateTaskPresenter):
+        format_called = False
+        format_called_with = {}
 
-    assert isinstance(response, CreateTaskOutputData)
-    assert response.task.description == description
-    assert task_repo_spy.description == description
+        def format(self, output_data: CreateTaskOutputData) -> None:
+            self.format_called = True
+            self.format_called_with = output_data
+
+    description = "Random Task"
+
+    input_data = CreateTaskInputData(description)
+
+    task_repo_spy = TaskRepoSpy()
+    presenter_spy = CreateTaskPresenterSpy()
+
+    usecase = CreateTask(task_repo_spy, presenter_spy)
+    usecase.execute(input_data)
+
+    assert task_repo_spy.new_task_called == True
+    assert task_repo_spy.new_task_called_with == description
+
+    assert presenter_spy.format_called == True
+    assert presenter_spy.format_called_with.task.description == description
