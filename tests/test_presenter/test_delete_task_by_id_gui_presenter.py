c0b9b70e7fdbe0c580ea45d099878498e2ad6579
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 16:14:17 -0400

    tests: introduce unit test for delete task by id gui presenter

---
+++ tests/test_presenter/test_delete_task_by_id_gui_presenter.py
@@ -0,0 +1,11 @@
+from src.todo_clean.layer2.presenter.delete_task_by_id_presenter import (
+    DeleteTaskByIdGuiPresenter,
+)
+
+
+def test_delete_task_by_id_gui_presenter():
+    success = True
+    presenter = DeleteTaskByIdGuiPresenter()
+    response = presenter.format(success)
+
+    assert response == {"success": success}
