ff9a70a8a7b62b3846d2241a178fd205927abefe
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 00:17:50 -0400

    refactor: correct input and output types of get tasks use case

--- src/todo_clean/layer1/usecase/get_tasks.py
+++ src/todo_clean/layer1/usecase/get_tasks.py
@@ -5,9 +5,14 @@ from ...layer0.entity.task import Task
 from ..repository.i_task_repo import ITaskRepo
 
 
+@dataclass
+class GetTasksOutputData:
+    tasks: list[Task]
+
+
 class IGetTasks(ABC):
     @abstractmethod
-    def execute(self) -> list[Task]:
+    def execute(self) -> GetTasksOutputData:
         pass
 
 
@@ -15,5 +20,6 @@ class GetTasks:
     def __init__(self, task_repo: ITaskRepo):
         self.task_repo = task_repo
 
-    def execute(self) -> list[Task]:
-        return self.task_repo.get_tasks()
+    def execute(self) -> GetTasksOutputData:
+        response = self.task_repo.get_tasks()
+        return GetTasksOutputData(response)
--- tests/test_usecase/test_get_tasks.py
+++ tests/test_usecase/test_get_tasks.py
@@ -1,11 +1,11 @@
 from src.todo_clean.layer0.entity.task import Task
 from src.todo_clean.layer1.repository.i_task_repo import ITaskRepo
-from src.todo_clean.layer1.usecase.get_tasks import GetTasks
+from src.todo_clean.layer1.usecase.get_tasks import GetTasks, GetTasksOutputData
 
 
 def test_get_tasks():
 
-    class SpyTaskRepo(ITaskRepo):
+    class TaskRepoSpy(ITaskRepo):
         task1 = Task(1, "Random Task")
         task2 = Task(2, "Random Task")
         task_list = [task1, task2]
@@ -25,7 +25,9 @@ def test_get_tasks():
         def delete_task_by_id(self, id_: int) -> bool:
             pass
 
-    task_repo = SpyTaskRepo()
+    task_repo = TaskRepoSpy()
     usecase = GetTasks(task_repo)
-    task_list = usecase.execute()
-    assert task_list == task_repo.task_list
+    output_data = usecase.execute()
+
+    assert isinstance(output_data, GetTasksOutputData)
+    assert output_data.tasks == task_repo.task_list
