d0b268c281de4baa050704b428abba7a44bde115
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 08:50:02 -0400

    tests: introduce unit test for create task gui presenter feat: implements create task gui presenter

---
+++ tests/test_presenter/test_create_task_gui_presenter.py
@@ -0,0 +1,10 @@
+from src.todo_clean.layer0.entity.task import Task
+from src.todo_clean.layer2.presenter.create_task_presenter import CreateTaskGuiPresenter
+
+
+def test_create_task_gui_presenter():
+    task = Task(1, "Random task description")
+    presenter = CreateTaskGuiPresenter()
+    response = presenter.format(task)
+
+    assert response == {"id": 1, "description": "Random task description"}
