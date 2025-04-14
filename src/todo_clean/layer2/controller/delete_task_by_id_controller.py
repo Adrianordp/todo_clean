8c3bd441271c3e3329bd010d393b74001d81a2e7
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 22:29:29 -0400

    feat: introduce delete task by id controller

---
+++ src/todo_clean/layer2/controller/delete_task_by_id_controller.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+from src.todo_clean.layer1.usecase.delete_task_by_id import (
+    DeleteTaskInputData,
+    IDeleteTaskById,
+)
+
+
+class IDeleteTaskByIdController(ABC):
+    @abstractmethod
+    def __init__(self, usecase: IDeleteTaskById):
+        pass
+
+    @abstractmethod
+    def handle(self, id_: int) -> None:
+        pass
+
+
+class DeleteTaskByIdController(IDeleteTaskByIdController):
+    def __init__(self, usecase: IDeleteTaskById):
+        self.usecase = usecase
+
+    def handle(self, id_: int) -> None:
+        input_data = DeleteTaskInputData(id_)
+        self.usecase.execute(input_data)
