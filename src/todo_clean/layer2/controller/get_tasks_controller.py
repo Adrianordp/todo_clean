4dbe91410cc5bc40445423bd8a36343591fb4647
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-13 22:44:42 -0400

    feat: introduce delete tasks controller

---
+++ src/todo_clean/layer2/controller/get_tasks_controller.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from src.todo_clean.layer1.usecase.get_tasks import IGetTasks
+
+
+class IGetTasksController(ABC):
+    @abstractmethod
+    def __init__(self, usecase: IGetTasks):
+        pass
+
+    @abstractmethod
+    def handle(self) -> None:
+        pass
+
+
+class GetTasksController(IGetTasksController):
+    def __init__(self, usecase: IGetTasks):
+        self.usecase = usecase
+
+    def handle(self) -> None:
+        self.usecase.execute()
