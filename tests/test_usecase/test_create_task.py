1b5b43ea89e77ab0a6932c703656d349e640cf7e
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-16 22:52:01 -0400

    test: unit test for create task

--- tests/test_usecase/test_create_task.py
+++ tests/test_usecase/test_create_task.py
@@ -1,58 +1,53 @@
-from src.todo_clean.layer0.entity.task import ITask
-from src.todo_clean.layer1.repository.i_task_repo import ITaskRepo
+"""Test use case for creating a task."""
+
+from src.todo_clean.layer0.entity.task import ITask, Task
 from src.todo_clean.layer1.usecase.create_task import (
     CreateTask,
-    CreateTaskInputData,
-    CreateTaskOutputData,
+    CreateTaskRequest,
+    CreateTaskResponse,
     ICreateTaskPresenter,
+    ICreateTaskRepository,
 )
 
 
+class RepositoryStub(ICreateTaskRepository):
+    """Spy for creating task repository."""
+
+    new_task_called = False
+    new_task_called_with = {}
+
+    def create_task(self, description: str) -> ITask:
+        self.new_task_called = True
+        self.new_task_called_with["description"] = description
+        return Task(1, description)
+
+
+class PresenterSpy(ICreateTaskPresenter):
+    """Spy for creating task presenter."""
+
+    format_called = False
+    format_called_with = {}
+
+    def format(self, output_data: CreateTaskResponse) -> None:
+        self.format_called = True
+        self.format_called_with["output_data"] = output_data
+
+
 def test_usecase_create_new_task():
-
-    class TaskRepoSpy(ITaskRepo):
-        new_task_called = False
-        new_task_called_with = {}
-
-        def create_task(self, description: str) -> ITask:
-            self.new_task_called = True
-            self.new_task_called_with["description"] = description
-
-        def get_task_by_id(self, id_: int) -> ITask:
-            pass
-
-        def get_tasks(self) -> list[ITask]:
-            pass
-
-        def edit_task_by_id(self, id_: int) -> ITask:
-            pass
-
-        def delete_task_by_id(self, id_: int) -> bool:
-            pass
-
-    class CreateTaskPresenterSpy(ICreateTaskPresenter):
-        format_called = False
-        format_called_with = {}
-
-        def format(self, output_data: CreateTaskOutputData) -> None:
-            self.format_called = True
-            self.format_called_with["output_data"] = output_data
-
+    """Test use case for creating a new task."""
     description = "Random ITask"
 
-    input_data = CreateTaskInputData(description)
+    request = CreateTaskRequest(description)
+    repository_stub = RepositoryStub()
+    presenter_spy = PresenterSpy()
 
-    task_repo_spy = TaskRepoSpy()
-    presenter_spy = CreateTaskPresenterSpy()
+    usecase = CreateTask(request, repository_stub, presenter_spy)
+    usecase.execute()
 
-    usecase = CreateTask(task_repo_spy, presenter_spy)
-    usecase.execute(input_data)
-
-    assert task_repo_spy.new_task_called is True
-    assert task_repo_spy.new_task_called_with["description"] == description
+    assert repository_stub.new_task_called is True
+    assert repository_stub.new_task_called_with["description"] == description
 
     assert presenter_spy.format_called is True
-    assert presenter_spy.format_called_with["output_data"] is not None
-    assert isinstance(
-        presenter_spy.format_called_with["output_data"], CreateTaskOutputData
-    )
+    assert presenter_spy.format_called_with[
+        "output_data"
+    ] == CreateTaskResponse(1, description)
