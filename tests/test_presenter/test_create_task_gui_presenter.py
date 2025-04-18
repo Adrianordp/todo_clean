afd0d71905f0f3afe453f86271c3020a7ad369e6
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-18 19:58:06 -0400

    tests: intriduce unit test for create task gui presenter

--- tests/test_presenter/test_create_task_gui_presenter.py
+++ tests/test_presenter/test_create_task_gui_presenter.py
@@ -1,12 +1,23 @@
-from src.todo_clean.layer0.entity.task import Task
+"""
+presenter/create_task_presenter.py
+
+Presenter for creating task use case."""
+
+from src.todo_clean.layer1.usecase.create_task import CreateTaskResponse
 from src.todo_clean.layer2.presenter.create_task_presenter import (
+    CreaTaskViewModel,
     CreateTaskGuiPresenter,
 )
 
 
 def test_create_task_gui_presenter():
-    task = Task(1, "Random task description")
+    """Test presenter for creating task use case."""
+    view_model = CreaTaskViewModel("1", "Random task description")
+    create_task_response = CreateTaskResponse(1, "Random task description")
     presenter = CreateTaskGuiPresenter()
-    response = presenter.format(task)
+    presenter.format(create_task_response)
 
-    assert response == {"id": 1, "description": "Random task description"}
+    assert isinstance(presenter.view_model, CreaTaskViewModel)
+    assert presenter.view_model.id == view_model.id
+    assert presenter.view_model.description == view_model.description
+    assert presenter.view_model == view_model
