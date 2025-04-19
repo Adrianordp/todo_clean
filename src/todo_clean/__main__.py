c60034b81bc0f99ae1ecc670dcb8510e00099144
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-18 21:13:44 -0400

    feat: introduce main script

--- src/todo_clean/__main__.py
+++ src/todo_clean/__main__.py
@@ -1,8 +1,30 @@
-from src.todo_clean.layer3.view.tkinter_view import TkinterView
+"""Main module."""
+
+import sqlite3
+from tkinter import Tk
+
+from todo_clean.layer1.repository.create_task_repository import (
+    CreateTaskSqliteRepository,
+)
+from todo_clean.layer1.usecase.create_task import CreateTask
+from todo_clean.layer2.controller.create_task_controller import (
+    CreateTaskController,
+)
+from todo_clean.layer2.presenter.create_task_presenter import (
+    CreateTaskGuiPresenter,
+)
+from todo_clean.layer3.view.tkinter_view import TkinterView
 
 
 def main():
-    root = TkinterView()
+    """Main function."""
+    conn = sqlite3.connect(":memory:")
+    create_task_repo = CreateTaskSqliteRepository(conn)
+    create_task_presenter = CreateTaskGuiPresenter()
+    create_task_use_case = CreateTask(create_task_repo, create_task_presenter)
+    controller = CreateTaskController(create_task_use_case)
+    root = TkinterView(Tk())
+    root.set_create_task_controller(controller)
     root.run()
 
 
