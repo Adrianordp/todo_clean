2b17283620a45b598677f52936e3ceb039fd52a6
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-18 20:28:25 -0400

    tests: intriduce unit test for create task controller

--- tests/test_controller/test_create_task_controller.py
+++ tests/test_controller/test_create_task_controller.py
@@ -1,61 +1,62 @@
+"""Test controller for creating a task."""
+
 from src.todo_clean.layer0.entity.task import ITask
-from src.todo_clean.layer1.repository.i_task_repo import ITaskRepo
 from src.todo_clean.layer1.usecase.create_task import (
-    CreateTaskInputData,
-    CreateTaskOutputData,
+    CreateTaskRequest,
+    CreateTaskResponse,
     ICreateTask,
     ICreateTaskPresenter,
+    ICreateTaskRepository,
 )
 from src.todo_clean.layer2.controller.create_task_controller import (
     CreateTaskController,
 )
 
 
-class TaskRepoDummy(ITaskRepo):
-    def create_task(self, description: str) -> ITask:
-        pass
+class CreateTaskRepositoryDummy(ICreateTaskRepository):
+    """Dummy for creating task repository."""
 
-    def get_task_by_id(self, _id: int) -> ITask:
-        pass
-
-    def get_tasks(self) -> list[ITask]:
-        pass
-
-    def edit_task_by_id(self, id_: int) -> ITask:
-        pass
-
-    def delete_task_by_id(self, id_: int) -> bool:
+    def create_task(self, description: str) -> tuple[int, ITask]:
         pass
 
 
 class CreateNewTaskSpy(ICreateTask):
+    """Spy for creating task use case."""
+
     execute_called = False
     execute_called_with = {}
 
-    def __init__(self, task_repo: ITaskRepo, presenter: ICreateTaskPresenter):
+    def __init__(
+        self, task_repo: ICreateTaskRepository, presenter: ICreateTaskPresenter
+    ):
         pass
 
-    def execute(self, input_data: CreateTaskInputData) -> None:
+    def execute(self, request: CreateTaskRequest) -> None:
         self.execute_called = True
-        self.execute_called_with["input_data"] = input_data
+        self.execute_called_with["request"] = request
 
 
 class PresenterDummy(ICreateTaskPresenter):
-    def format(self, output_data: CreateTaskOutputData) -> dict:
+    """Dummy for creating task presenter."""
+
+    def format(self, output_data: CreateTaskResponse) -> None:
         pass
 
 
 def test_controller_create_task():
+    """Test controller for creating a task."""
     description = "Random task description"
-    task_repo_dummy = TaskRepoDummy()
+    task_repo_stub = CreateTaskRepositoryDummy()
     presenter_dummy = PresenterDummy()
 
-    usecase_spy = CreateNewTaskSpy(task_repo_dummy, presenter_dummy)
+    usecase_spy = CreateNewTaskSpy(task_repo_stub, presenter_dummy)
     controller = CreateTaskController(usecase_spy)
     controller.handle(description)
 
     assert usecase_spy.execute_called is True
+    assert isinstance(
+        usecase_spy.execute_called_with["request"], CreateTaskRequest
+    )
     assert (
-        usecase_spy.execute_called_with["input_data"].description
-        == description
+        usecase_spy.execute_called_with["request"].description == description
     )
