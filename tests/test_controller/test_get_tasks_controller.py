64b6cb8a6070d6d53ec9d5b39f1de21b1a028836
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-25 20:03:30 -0400

    tests[test_get_tasks_controller.py]: introduce unit tests

--- tests/test_controller/test_get_tasks_controller.py
+++ tests/test_controller/test_get_tasks_controller.py
@@ -1,54 +1,61 @@
-# from todo_clean.layer0.entity.task import ITask
-# from todo_clean.layer1.repository.i_task_repo import ITaskRepo
-# from todo_clean.layer1.usecase.get_tasks import (
-#     GetTasksOutputData,
-#     IGetTasks,
-# )
-# from todo_clean.layer2.controller.get_tasks_controller import (
-#     GetTasksController,
-# )
+"""
+Test controller for getting tasks.
+"""
+
+from todo_clean.layer0.entity.task import ITask
+from todo_clean.layer1.usecase.get_tasks import (
+    GetTasksResponse,
+    IGetTasks,
+    IGetTasksPresenter,
+    IGetTasksRepository,
+)
+from todo_clean.layer2.controller.get_tasks_controller import (
+    GetTasksController,
+)
 
 
-# class TaskRepoSpy(ITaskRepo):
-#     def create_task(self, description: str) -> ITask:
-#         pass
+class RepositoryDummy(IGetTasksRepository):
+    """
+    Dummy repository for testing.
+    """
 
-#     def get_task_by_id(self, _id: int) -> ITask:
-#         pass
-
-#     def get_tasks(self) -> list[ITask]:
-#         pass
-
-#     def edit_task_by_id(self, id_: int) -> ITask:
-#         pass
-
-#     def delete_task_by_id(self, id_: int) -> bool:
-#         pass
+    def get_tasks(self) -> list[ITask]:
+        pass
 
 
-# class GetTasksSpy(IGetTasks):
-#     init_called = False
-#     init_called_with = {}
-#     execute_called = False
-#     execute_called_with = {}
+class PresenterDummy(IGetTasksPresenter):
+    """
+    Dummy presenter for testing.
+    """
 
-#     def __init__(self, task_repo: ITaskRepo):
-#         self.init_called = True
-#         self.init_called_with["task_repo"] = task_repo
-
-#     def execute(self) -> None:
-#         self.execute_called = True
+    def format(self, _: GetTasksResponse) -> None:
+        pass
 
 
-# def test_controller_get_task_by_id():
-#     task_repo_spy = TaskRepoSpy()
-#     usecase_spy = GetTasksSpy(task_repo_spy)
+class UseCaseSpy(IGetTasks):
+    """
+    Spy for creating task use case.
+    """
 
-#     controller = GetTasksController(usecase_spy)
+    execute_called = False
 
-#     controller.handle()
+    def __init__(self, _: IGetTasksRepository, __: IGetTasksPresenter):
+        pass
 
-#     assert usecase_spy.init_called is True
-#     assert usecase_spy.init_called_with["task_repo"] == task_repo_spy
+    def execute(self) -> None:
+        self.execute_called = True
 
-#     assert usecase_spy.execute_called is True
+
+def test_get_tasks_controller():
+    """
+    Test controller for getting tasks.
+    """
+    repository_dummy = RepositoryDummy()
+    presenter_dummy = PresenterDummy()
+    usecase_spy = UseCaseSpy(repository_dummy, presenter_dummy)
+
+    controller = GetTasksController(usecase_spy)
+
+    controller.handle()
+
+    assert usecase_spy.execute_called is True
