54510bd4b573f5e39391d634c5008b50bd58d612
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-25 20:56:43 -0400

    chore: delete unused files

--- src/todo_clean/layer2/presenter/delete_task_by_id_presenter.py
+++
@@ -1,8 +0,0 @@
-from todo_clean.layer1.usecase.delete_task_by_id import (
-    IDeleteTaskByIdPresenter,
-)
-
-
-class DeleteTaskByIdGuiPresenter(IDeleteTaskByIdPresenter):
-    def format(self, output_data: bool) -> None:
-        return {"success": output_data}
--- src/todo_clean/layer2/presenter/get_task_by_id_presenter.py
+++
@@ -1,7 +0,0 @@
-from todo_clean.layer0.entity.task import ITask
-from todo_clean.layer1.usecase.get_task_by_id import IGetTaskByIdPresenter
-
-
-class GetTaskByIdGuiPresenter(IGetTaskByIdPresenter):
-    def format(self, output_data: ITask) -> dict:
-        return {"id": output_data.id_, "description": output_data.description}
