d1b28e782ad88ce9133e8988cda080ab99a55d7a
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-26 16:16:12 -0400

    feat[delete_task_by_id_repository.py]: introduce implementation for delete task by id repository

---
+++ src/todo_clean/layer3/repository/delete_task_by_id_repository.py
@@ -0,0 +1,28 @@
+"""
+Implementation of IDeleteTaskByIdRepository.
+"""
+
+import sqlite3
+from contextlib import closing
+
+from todo_clean.layer1 import IDeleteTaskByIdRepository
+
+
+class DeleteTaskByIdSqliteRepository(IDeleteTaskByIdRepository):
+    """
+    Implementation of IDeleteTaskByIdRepository for SQLite.
+
+    :param sqlite3.Connection conn: The connection to the SQLite database.
+    """
+
+    def __init__(self, conn: sqlite3.Connection):
+        self.conn = conn
+
+    def delete_task_by_id(self, id_: int) -> bool:
+        with closing(self.conn.cursor()) as cursor:
+            try:
+                cursor.execute("DELETE FROM tasks WHERE id = ?", (id_,))
+                self.conn.commit()
+            except sqlite3.OperationalError:
+                return False
+        return True
