59664522fd409acb7a2fb4dcca59839ed361996c
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-26 16:14:26 -0400

    chore[__init__.py]: improve module imports

--- src/todo_clean/layer1/__init__.py
+++ src/todo_clean/layer1/__init__.py
@@ -4,5 +4,29 @@ Layer 1
 Layer for use cases.
 """
 
-from .usecase.create_task import CreateTask, ICreateTask
-from .usecase.get_tasks import GetTasks, IGetTasks
+from .usecase.create_task import (
+    CreaTaskViewModel,
+    CreateTask,
+    CreateTaskRequest,
+    CreateTaskResponse,
+    ICreateTask,
+    ICreateTaskPresenter,
+    ICreateTaskRepository,
+)
+from .usecase.delete_task_by_id import (
+    DeleteTaskById,
+    DeleteTaskByIdRequest,
+    DeleteTaskByIdResponse,
+    DeleteTaskByIdViewModel,
+    IDeleteTaskById,
+    IDeleteTaskByIdPresenter,
+    IDeleteTaskByIdRepository,
+)
+from .usecase.get_tasks import (
+    GetTasks,
+    GetTasksResponse,
+    GetTasksViewModel,
+    IGetTasks,
+    IGetTasksPresenter,
+    IGetTasksRepository,
+)
--- src/todo_clean/layer3/__init__.py
+++ src/todo_clean/layer3/__init__.py
@@ -5,5 +5,8 @@ Layer for views and repositories.
 """
 
 from .repository.create_task_repository import CreateTaskSqliteRepository
+from .repository.delete_task_by_id_repository import (
+    DeleteTaskByIdSqliteRepository,
+)
 from .repository.get_tasks_repository import GetTasksSqliteRepository
 from .view.tkinter_view import TkinterView
