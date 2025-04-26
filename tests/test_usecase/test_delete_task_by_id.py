bbeab3a50041ce32d03f6317b43c9efadf8d789f
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-26 15:55:16 -0400

    tests[test_delete_task_by_id.py]: introduce unit tests

--- tests/test_usecase/test_delete_task_by_id.py
+++ tests/test_usecase/test_delete_task_by_id.py
@@ -1,58 +1,102 @@
-# from pytest import mark
+"""
+Test use case for deleting a task.
+"""
 
-# from todo_clean.layer0.entity.task import ITask
-# from todo_clean.layer1.usecase.delete_task_by_id import (
-#     DeleteTaskById,
-#     DeleteTaskByIdInputData,
-#     DeleteTaskByIdOutputData,
-#     IDeleteTaskByIdPresenter,
-# )
+from todo_clean.layer1.usecase.delete_task_by_id import (
+    DeleteTaskById,
+    DeleteTaskByIdRequest,
+    DeleteTaskByIdResponse,
+    IDeleteTaskByIdPresenter,
+    IDeleteTaskByIdRepository,
+)
 
 
-# @mark.skip
-# def test_usecase_delete_task_by_id():
+class RepositoryStub(IDeleteTaskByIdRepository):
+    """
+    Stub for repository for deleting a task.
+    """
 
-#     class TaskRepoSpy(ITaskRepo):
-#         delete_task_by_id_called = False
-#         delete_task_by_id_called_with = {}
+    def __init__(self, fake_success: bool = True):
+        self.delete_task_by_id_called = False
+        self.delete_task_by_id_called_with = {}
+        self.fake_success = fake_success
 
-#         def create_task(self, description: str) -> ITask:
-#             pass
+    def delete_task_by_id(self, id_: int) -> bool:
+        self.delete_task_by_id_called = True
+        self.delete_task_by_id_called_with["id_"] = id_
+        return self.fake_success
 
-#         def get_task_by_id(self, id_: int) -> ITask:
-#             pass
 
-#         def get_tasks(self) -> list[ITask]:
-#             pass
+class DeleteTaskPresenterSpy(IDeleteTaskByIdPresenter):
+    """
+    Spy for presenter for deleting a task.
+    """
 
-#         def edit_task_by_id(self, id_: int) -> ITask:
-#             pass
+    format_called = False
+    format_called_with = {}
 
-#         def delete_task_by_id(self, id_: int) -> bool:
-#             self.delete_task_by_id_called = True
-#             self.delete_task_by_id_called_with["id_"] = id_
+    def format(self, response: DeleteTaskByIdResponse) -> None:
+        self.format_called = True
+        self.format_called_with["response"] = response
 
-#     class DeleteTaskPresenterSpy(IDeleteTaskByIdPresenter):
-#         format_called = False
-#         format_called_with = {}
 
-#         def format(self, output_data: DeleteTaskByIdOutputData) -> None:
-#             self.format_called = True
-#             self.format_called_with["output_data"] = output_data
+def test_usecase_delete_task_by_id():
+    """
+    Test use case for deleting a task.
+    """
+    id_ = 1
+    success = True
 
-#     id_ = 1
-#     presenter_spy = DeleteTaskPresenterSpy()
-#     input_data = DeleteTaskByIdInputData(id_)
-#     task_repo_spy = TaskRepoSpy()
-#     usecase = DeleteTaskById(task_repo_spy, presenter_spy)
-#     usecase.execute(input_data)
+    presenter_spy = DeleteTaskPresenterSpy()
+    request = DeleteTaskByIdRequest(id_)
+    task_repo_spy = RepositoryStub(success)
 
-#     assert task_repo_spy.delete_task_by_id_called
-#     assert task_repo_spy.delete_task_by_id_called_with["id_"] == id_
+    usecase = DeleteTaskById(task_repo_spy, presenter_spy)
+    usecase.execute(request)
 
-#     assert presenter_spy.format_called is True
-#     assert presenter_spy.format_called_with["output_data"] is not None
-#     assert isinstance(
-#         presenter_spy.format_called_with["output_data"],
-#         DeleteTaskByIdOutputData,
-#     )
+    assert task_repo_spy.delete_task_by_id_called is True
+    assert task_repo_spy.delete_task_by_id_called_with["id_"] == id_
+
+    assert presenter_spy.format_called is True
+    assert presenter_spy.format_called_with["response"] is not None
+    assert isinstance(
+        presenter_spy.format_called_with["response"],
+        DeleteTaskByIdResponse,
+    )
+    assert presenter_spy.format_called_with["response"].success is success
+    assert presenter_spy.format_called_with[
+        "response"
+    ] == DeleteTaskByIdResponse(success)
+
+    assert usecase.get_presenter() == presenter_spy
+
+
+def test_usecase_delete_task_by_id_unhappy():
+    """
+    Test use case for deleting a task.
+    """
+    id_ = 1
+    success = False
+
+    presenter_spy = DeleteTaskPresenterSpy()
+    request = DeleteTaskByIdRequest(id_)
+    task_repo_spy = RepositoryStub(success)
+
+    usecase = DeleteTaskById(task_repo_spy, presenter_spy)
+    usecase.execute(request)
+
+    assert task_repo_spy.delete_task_by_id_called is True
+    assert task_repo_spy.delete_task_by_id_called_with["id_"] == id_
+
+    assert presenter_spy.format_called is True
+    assert presenter_spy.format_called_with["response"] is not None
+    assert isinstance(
+        presenter_spy.format_called_with["response"],
+        DeleteTaskByIdResponse,
+    )
+    assert presenter_spy.format_called_with["response"].success is success
+    assert presenter_spy.format_called_with[
+        "response"
+    ] == DeleteTaskByIdResponse(success)
+
+    assert usecase.get_presenter() == presenter_spy
