1f6705813bde8506746c742eab857dde9521ac44
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-27 18:00:35 -0400

    feat[__main__.py]: introduce delete task use case

--- src/todo_clean/__main__.py
+++ src/todo_clean/__main__.py
@@ -3,15 +3,18 @@
 import sqlite3
 from tkinter import Tk
 
-from todo_clean.layer1 import CreateTask, GetTasks
+from todo_clean.layer1 import CreateTask, DeleteTaskById, GetTasks
 from todo_clean.layer2 import (
     CreateTaskController,
     CreateTaskGuiPresenter,
+    DeleteTaskByIdController,
+    DeleteTaskByIdGuiPresenter,
     GetTasksController,
     GetTasksGuiPresenter,
 )
 from todo_clean.layer3 import (
     CreateTaskSqliteRepository,
+    DeleteTaskByIdSqliteRepository,
     GetTasksSqliteRepository,
     TkinterView,
 )
@@ -26,6 +29,13 @@ def main():
     create_task_use_case = CreateTask(create_task_repo, create_task_presenter)
     create_task_controller = CreateTaskController(create_task_use_case)
 
+    delete_task_repo = DeleteTaskByIdSqliteRepository(conn)
+    delete_task_presenter = DeleteTaskByIdGuiPresenter()
+    delete_task_use_case = DeleteTaskById(
+        delete_task_repo, delete_task_presenter
+    )
+    delete_task_controller = DeleteTaskByIdController(delete_task_use_case)
+
     get_tasks_repo = GetTasksSqliteRepository(conn)
     get_tasks_presenter = GetTasksGuiPresenter()
     get_tasks_use_case = GetTasks(get_tasks_repo, get_tasks_presenter)
@@ -34,6 +44,7 @@ def main():
     root = TkinterView(Tk())
     root.set_create_task_controller(create_task_controller)
     root.set_get_tasks_controller(get_tasks_controller)
+    root.set_delete_task_controller(delete_task_controller)
     root.run()
 
 
