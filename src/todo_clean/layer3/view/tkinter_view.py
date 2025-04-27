8507712425aa37b5f89d6b22dad3afa19af30070
Adriano Rodrigues <adrianordp1@gmail.com>
2025-04-27 17:59:51 -0400

    feat[tkinter_view.py]: introduce delete task use case

--- src/todo_clean/layer3/view/tkinter_view.py
+++ src/todo_clean/layer3/view/tkinter_view.py
@@ -5,7 +5,11 @@ Tkinter view for the TODO Clean application.
 import json
 from tkinter import Button, Entry, Listbox, Tk
 
-from todo_clean.layer2 import CreateTaskController, GetTasksController
+from todo_clean.layer2 import (
+    CreateTaskController,
+    DeleteTaskByIdController,
+    GetTasksController,
+)
 
 
 class TkinterView:
@@ -21,22 +25,31 @@ class TkinterView:
 
         self.create_task_controller: CreateTaskController
         self.get_tasks_controller: GetTasksController
+        self.delete_task_controller: DeleteTaskByIdController
 
         self.root.title("TODO Clean")
         self.root.geometry("400x400")
+
+        self.task_array = []
+        self.id_array = []
 
         self.create_task_entry = Entry(self.root)
         self.create_task_entry.pack()
 
         self.create_task_button = Button(
-            self.root, text="Create Task", command=self.handle_create_task
+            self.root, text="Create Task", command=self._handle_create_task
         )
         self.create_task_button.pack()
+
+        self.delete_task_button = Button(
+            self.root, text="Delete Task", command=self._handle_delete_task
+        )
+        self.delete_task_button.pack()
 
         self.task_list = Listbox(self.root)
         self.task_list.pack()
 
-    def handle_create_task(self) -> None:
+    def _handle_create_task(self) -> None:
         """
         Handle creating a task.
         """
@@ -44,9 +57,20 @@ class TkinterView:
 
         self.create_task_controller.handle(description)
 
-        self.handle_get_tasks()
+        self._handle_get_tasks()
 
-    def handle_get_tasks(self) -> None:
+    def _handle_delete_task(self) -> None:
+        """
+        Handle deleting a task.
+        """
+        if len(self.task_list.curselection()) == 0:
+            return
+        idx = self.task_list.curselection()[0]
+        id_ = self.id_array[idx]
+        self.delete_task_controller.handle(id_)
+        self._handle_get_tasks()
+
+    def _handle_get_tasks(self) -> None:
         """
         Handle getting tasks.
         """
@@ -54,11 +78,12 @@ class TkinterView:
         presenter = self.get_tasks_controller.usecase.get_presenter()
         view_model = presenter.get_view_model()
         tasks_json: dict = json.loads(view_model.tasks)
-        tasks_list = [
+        self.task_array = [
             description["description"] for _, description in tasks_json.items()
         ]
+        self.id_array = [id for id, _ in tasks_json.items()]
         self.task_list.delete(0, "end")
-        self.task_list.insert(0, *tasks_list)
+        self.task_list.insert(0, *self.task_array)
 
     def run(self) -> None:
         """
@@ -74,10 +99,18 @@ class TkinterView:
         """
         self.create_task_controller = controller
 
+    def set_delete_task_controller(
+        self, controller: DeleteTaskByIdController
+    ) -> None:
+        """
+        Set the delete task controller.
+        """
+        self.delete_task_controller = controller
+
     def set_get_tasks_controller(self, controller: GetTasksController) -> None:
         """
         Set the get tasks controller.
         """
         self.get_tasks_controller = controller
 
-        self.handle_get_tasks()
+        self._handle_get_tasks()
